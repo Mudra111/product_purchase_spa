a65900a7b7b4404787885dc2bb8f0516f8b0cb8f
mudra <mudrabhalodiya@gmail.com>
2024-10-17 01:59:51 +0530

    deployment error correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => (
   <footer className="footerMain">
     <div>
       <p>&copy; 2024 XYZ company</p>
-      <a href="#" className="fa fa-facebook"></a>
-      <a href="#" className="fa fa-twitter"></a>
-      <a href="#" className="fa fa-google"></a>
-      <a href="#" className="fa fa-linkedin"></a>
+      <p className="fa fa-facebook"></p>
+      <p className="fa fa-twitter"></p>
+      <p className="fa fa-google"></p>
+      <p className="fa fa-linkedin"></p>
     </div>
   </footer>
 );
